780dee3b23cf84f7547ad0054b2c45cfd0c1ae9c
rokuirie <akenagibbsherbert6@gmail.com>
2021-05-22 23:50:09 +0300

    changed from Bootstrap toggle btn icon to react-fontawesome icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+// font-awesome import
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
@@ -7,7 +10,7 @@ const Navbar = () => {
       <div className="container">
       <Link className="navbar-brand" to="#">rokku</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+        <FontAwesomeIcon icon={faBars} style={{color: "#fff"}}/>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ml-auto">
