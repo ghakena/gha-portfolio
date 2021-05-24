b25bfea3797cd225e0ae87e163d39fc9a305b2a0
rokuirie <akenagibbsherbert6@gmail.com>
2021-05-24 09:42:42 +0300

    completed about section

--- src/components/About.js
+++ src/components/About.js
@@ -7,15 +7,15 @@ const About = () => {
         <h1 className="display-4">About Me</h1>
         <div className="container">
           <div className="row padding">
-            <div className="col-md-5">
+            <div className="col-sm-12 col-md-5">
               <img src="https://res.cloudinary.com/rokuirie/image/upload/v1621762494/Portfolio/charles-deluvio-pjAH2Ax4uWk-unsplash_vlo4kp_1_1_vsry9l.jpg" class="img-thumbnail" alt="about-img" />
               <br />
             </div>
-            <div className="col-md-7 text-justify">
-                I am Gibbs, 23 and a 2021 Computer Engineering graduate.
+            <div className="col-sm-12 col-md-7 text-justify">
+                I'm Gibbs, 23 and a 2021 Computer Engineering graduate.
                 I have great passion for working on projects in the fields of web development and data science.
                 I work with and I'm well-versed with ReactJS for building web applications.
-                For data science projects, I work with Python as my tool of choice.
+                For data science projects, I work with Python as my tool of choice. I build fully-scalable websites and web applications based off of design choices agreed upon by the client. I also take on data analysis, machine learning and deep learning-oriented projects in the areas of fraud detection, risk analysis, credit-scoring, among others.
             </div>
           </div>
         </div>
