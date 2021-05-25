85436998db200b5e829cd6858dab7c04b92ae0ea
rokuirie <akenagibbsherbert6@gmail.com>
2021-05-25 11:30:17 +0300

    completed contact form design, and validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,42 +1,66 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { TextField } from './TextField';
+import { MessageField } from './MessageField';
+import '../App.css';
+import * as Yup from 'yup';
 
 const Contact = () => {
+
+  const validate = Yup.object({
+    name: Yup.string()
+      .min(5, "Must be at least 5 characters")
+      .required("Name is required"),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    message: Yup.string()
+      .min(4, "Must be at least 4 characters.")
+      .required("Message is required")
+  })
+
   return (
-    <div className="contact padding">
-      <div className="col-md-12 text-center">
-          <h1>Contact Me</h1>
-        </div>
-      <div className="container-fluid">
-        <form className="contact-form">
-          {/* <!-- Name input --> */}
-          <div class="form-outline mb-4">
-            <label class="form-label" for="form4Example1">Name</label>
-            <input type="text" id="form4Example1" class="form-control"/>
+    <Formik
+      initialValues={{
+        name: '',
+        email: '',
+        message: ''
+      }}
+      validationSchema={validate}
+      onSubmit={values => {
+        console.log(values)
+      }}
+    >
+      {formik => (
+        <div className="contact padding">
+          <h1 className="mt-4 text-center">Contact Me</h1>
+          <div className="container">
+
+          <div className="row padding">
+            <div className="col-md-3">
+              
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <Form className="form">
+                <TextField label="Name" name="name" type="text" />
+                <TextField label="Email Address" name="email" type="email" />
+                <MessageField label="Message" name="message" type="text" />
+                <button className="btn mt-3" type="submit">Submit</button>
+              </Form>
+            </div>
+            <div className="col-md-3">
+              
+            </div>
+          </div>
           </div>
 
-          {/* <!-- Email input --> */}
-          <div class="form-outline mb-4">
-            <label class="form-label" for="form4Example2">Email address</label>
-            <input type="email" id="form4Example2" class="form-control"/>
-          </div>
-
-          {/* <!-- Message input --> */}
-          <div class="form-outline mb-4">
-            <label class="form-label" for="form4Example3">Message</label>
-            <textarea class="form-control" id="form4Example3" rows="4">
-            </textarea>
-          </div>
           
-          {/* <!-- Submit button --> */}
-          <button type="submit" class="btn mb-4">Send</button>
-        </form>
-      </div>
-      
-      
-    </div>
+          
+        </div>
+      )}
+
+    </Formik>
   )
 }
 
-export default Contact
+export default Contact;
