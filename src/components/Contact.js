bf0456e7fc58ede46a28a84fd8283ea8165614a6
rokuirie <akenagibbsherbert6@gmail.com>
2021-05-25 12:28:53 +0300

    completed form submission handling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,7 @@ import { TextField } from './TextField';
 import { MessageField } from './MessageField';
 import '../App.css';
 import * as Yup from 'yup';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
 
@@ -15,7 +16,6 @@ const Contact = () => {
       .email("Invalid email address")
       .required("Email is required"),
     message: Yup.string()
-      .min(4, "Must be at least 4 characters.")
       .required("Message is required")
   })
 
@@ -27,35 +27,47 @@ const Contact = () => {
         message: ''
       }}
       validationSchema={validate}
-      onSubmit={values => {
-        console.log(values)
-      }}
+      onSubmit={(values, {setSubmitting, resetForm})=>{
+        setTimeout(()=>{
+          const templateParams = {
+            from_name: values.name + " (" + values.email + ")",
+            to_name: "Gibbs Herbert Akena",
+            message: values.message
+          }
+          emailjs.send("service_gd0v61s", "template_9cmmznv", 
+            templateParams, "user_KDK7wflsBaBv97zUBR9YX")
+            .then((response)=>{
+              alert('Your message has been sent!')
+              resetForm()
+              setSubmitting(false)
+            }, (err) => {
+              console.log(err)
+            })
+        }, 2000)
+    }}
     >
-      {formik => (
+      {props => (
         <div className="contact padding">
           <h1 className="mt-4 text-center">Contact Me</h1>
           <div className="container">
-
-          <div className="row padding">
-            <div className="col-md-3">
-              
-            </div>
-            <div className="col-sm-12 col-md-6">
-              <Form className="form">
-                <TextField label="Name" name="name" type="text" />
-                <TextField label="Email Address" name="email" type="email" />
-                <MessageField label="Message" name="message" type="text" />
-                <button className="btn mt-3" type="submit">Submit</button>
-              </Form>
-            </div>
-            <div className="col-md-3">
-              
+            <div className="row padding">
+              <div className="col-md-3">
+              </div>
+              <div className="col-sm-12 col-md-6">
+                <Form className="form">
+                  <TextField label="Name" name="name" type="text" />
+                  <TextField label="Email Address" name="email" type="email" />
+                  <MessageField label="Message" name="message" type="text" />
+                  <button className="btn mt-3" type="submit">
+                    {/* Submit */}
+                    {props.isSubmitting ? "SUBMITTING...": "SUBMIT"}
+                  </button>
+                </Form>
+              </div>
+              <div className="col-md-3">
+              </div>
             </div>
           </div>
-          </div>
-
-          
-          
         </div>
       )}
 
